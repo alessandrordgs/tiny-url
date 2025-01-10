645821fe0990e1c4f3f324fb07cf587f35202f25
alessandrordgs <ale.silva1120@gmail.com>
2025-01-10 17:58:48 -0300

    feat(Swagger): add swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('URl shortener API')
+    .setDescription('Documentation for the URL shortener API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
